e0c422b8835bf66d1f5fbf9fe0312b9200953c19
sariii29 <101092791+sariii29@users.noreply.github.com>
2022-06-27 18:52:58 +0800

    Read, Search, Delete pada AlamatViewFrame

--- src/main/java/frame/AlamatViewFrame.java
+++ src/main/java/frame/AlamatViewFrame.java
@@ -1,15 +1,21 @@
 package frame;
 
+import helpers.Koneksi;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.*;
 
 public class AlamatViewFrame extends JFrame{
     private JPanel mainPanel;
     private JPanel cariPanel;
     private JScrollPane viewScrollPane;
     private JPanel buttonPanel;
-    private JTextField textField1;
+    private JTextField cariTextField;
     private JButton cariButton;
-    private JLabel CariTextField;
     private JTable viewTable;
     private JButton tambahButton;
     private JButton ubahButton;
@@ -19,6 +25,64 @@ public class AlamatViewFrame extends JFrame{
     private JButton tutupButton;
 
     public AlamatViewFrame(){
+        tutupButton.addActionListener(e -> {
+            dispose();
+        });
+        batalButton.addActionListener(e -> {
+            isiTable();
+        });
+        addWindowListener(new WindowAdapter() {
+        @Override
+        public void windowActivated(WindowEvent e) {
+            isiTable();
+            }
+        });
+        cariButton.addActionListener(e -> {
+            Connection c = Koneksi.getConnection();
+            String keyword = "%" + cariTextField.getText() + "%";
+            String searchSQL = "SELECT * FROM alamat WHERE nama like ?";
+            try {
+                PreparedStatement ps = c.prepareStatement(searchSQL);
+                ps.setString(1, keyword);
+                ResultSet rs = ps.executeQuery();
+                DefaultTableModel dtm = (DefaultTableModel) viewTable.getModel();
+                dtm.setRowCount(0);
+                Object[] row = new Object[2];
+                while (rs.next()) {
+                    row[0] = rs.getInt("id");
+                    row[1] = rs.getString("nama");
+                    dtm.addRow(row);
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        hapusButton.addActionListener(e -> {
+            int barisTerpilih = viewTable.getSelectedRow();
+            if (barisTerpilih < 0){
+                JOptionPane.showMessageDialog(null,"Pilih data dulu");
+                return;
+            }
+            int pilihan = JOptionPane.showConfirmDialog(null,
+                    "Yakin mau hapus?",
+                    "Konfirmasi Hapus",
+                    JOptionPane.YES_NO_OPTION
+            );
+            if (pilihan == 0){
+                TableModel tm = viewTable.getModel();
+                int id = Integer.parseInt(tm.getValueAt(barisTerpilih,0).toString());
+                Connection c = Koneksi.getConnection();
+                String deleteSQL = "DELETE FROM alamat WHERE id = ?";
+                try {
+                    PreparedStatement ps = c.prepareStatement(deleteSQL);
+                    ps.setInt(1, id);
+                    ps.executeUpdate();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        isiTable();
         init();
     }
 
@@ -28,5 +92,25 @@ public class AlamatViewFrame extends JFrame{
         pack();
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
+    }
+
+    public void isiTable(){
+        Connection c = Koneksi.getConnection();
+        String selectSQL = "SELECT * FROM alamat";
+        try {
+            Statement s = c.createStatement();
+            ResultSet rs = s.executeQuery(selectSQL);
+            String header[] = {"Id","Nama Alamat"};
+            DefaultTableModel dtm = new DefaultTableModel(header,0);
+            viewTable.setModel(dtm);
+            Object[] row = new Object[2];
+            while (rs.next()){
+                row[0] = rs.getInt("id");
+                row[1] = rs.getString("nama");
+                dtm.addRow(row);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
